1a9d93fc72c3a40f4d02d5bf1a53d7814e02feb0
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-09-09 20:42:40 +0430

    secureArea.js and secureSection.js added to IndexPage.js

--- src/js/views/pages/IndexPage.js
+++ src/js/views/pages/IndexPage.js
@@ -1,14 +1,17 @@
 const React = require('react');
 const LoginSection = require('./components/LoginSection');
+const SecureSection = require('./components/SecureSection');
 
-class IndexPage extends React.Component{
+class IndexPage extends React.Component {
 
-    render(){
-        return (<React.Fragment>
-            <LoginSection />
-        </React.Fragment>
-            );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <LoginSection/>
+        <SecureSection/>
+      </React.Fragment>
+    );
+  }
 
 }
 
